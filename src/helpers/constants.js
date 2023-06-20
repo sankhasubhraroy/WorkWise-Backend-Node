2624d4998f624987992905ed35b80bad5fba22e0
Akash Mukherjee <akash3.11.2000@gmail.com>
2023-06-20 12:16:21 +0530

    validation regexes are added to constants

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,9 +1,26 @@
+// User Roles
 const ROLE = {
   ADMIN: "admin",
   CONSUMER: "consumer",
   FREELANCER: "freelancer",
 };
 
+// Avatar Image Size for DiceBear Avatars
+const AVATAR_IMAGE_SIZE = 200;
+
+// Regular Expressions for Validations
+const NAME_REGEX = /^[a-zA-Z-]+$/
+const EMAIL_REGEX = /^[a-z0-9]+@[a-z]+\.[a-z]{2,3}$/
+const USERNAME_REGEX = /^[a-zA-Z0-9]+$/
+const PHONE_REGEX = /^((\+91)?|91?|0)?[789][0-9]{9}$/
+const PASSWORD_REGEX = /^(?=.[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+
 module.exports = {
   ROLE,
+  AVATAR_IMAGE_SIZE,
+  NAME_REGEX,
+  EMAIL_REGEX,
+  USERNAME_REGEX,
+  PHONE_REGEX,
+  PASSWORD_REGEX,
 };
--- src/helpers/validations.js
+++ src/helpers/validations.js
@@ -1,58 +1,51 @@
-const nameRegex = new RegExp(process.env.NAME_REGEX);
-const emailRegex = new RegExp(process.env.EMAIL_REGEX);
-const usernameRegex = new RegExp(process.env.USERNAME_REGEX);
-const phoneRegex = new RegExp(process.env.PHONE_REGEX);
-const passwordRegex = new RegExp(process.env.PASSWORD_REGEX);
+const {
+  NAME_REGEX,
+  EMAIL_REGEX,
+  USERNAME_REGEX,
+  PHONE_REGEX,
+  PASSWORD_REGEX,
+} = require("./constants");
 
 exports.isNameValid = (name) => {
-    if (name.length < 3)
-        return false;
+  if (name.length < 3) return false;
 
-    const isValid = nameRegex.test(name);
-    if (!isValid)
-        return false;
+  const isValid = NAME_REGEX.test(name);
+  if (!isValid) return false;
 
-    return true;
-}
+  return true;
+};
 
 exports.isEmailValid = (email) => {
-    if (email.length > 254)
-        return false;
+  if (email.length > 254) return false;
 
-    const isValid = emailRegex.test(email);
-    if (!isValid)
-        return false;
+  const isValid = EMAIL_REGEX.test(email);
+  if (!isValid) return false;
 
-    const parts = email.split("@");
-    if (parts[0].length > 64)
-        return false;
+  const parts = email.split("@");
+  if (parts[0].length > 64) return false;
 
-    return true;
-}
+  return true;
+};
 
 exports.isUsernameValid = (username) => {
-    if (username.length < 3)
-        return false;
+  if (username.length < 3) return false;
 
-    const isValid = usernameRegex.test(username);
-    if (!isValid)
-        return false;
+  const isValid = USERNAME_REGEX.test(username);
+  if (!isValid) return false;
 
-    return true;
-}
+  return true;
+};
 
 exports.isPhoneValid = (phone) => {
-    const isValid = phoneRegex.test(phone);
-    if (!isValid)
-        return false;
+  const isValid = PHONE_REGEX.test(phone);
+  if (!isValid) return false;
 
-    return true;
-}
+  return true;
+};
 
 exports.isPasswordValid = (password) => {
-    const isValid = passwordRegex.test(password);
-    if (!isValid)
-        return false;
+  const isValid = PASSWORD_REGEX.test(password);
+  if (!isValid) return false;
 
-    return true;
-}
+  return true;
+};
