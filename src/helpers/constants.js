08c1492a82c58f14c58bf97f84675d5921162c73
Akash Mukherjee <akash3.11.2000@gmail.com>
2023-06-21 22:30:03 +0530

    minute changes in helper modules and addition of new Models

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -15,6 +15,11 @@ const USERNAME_REGEX = /^[a-zA-Z0-9]+$/
 const PHONE_REGEX = /^((\+91)?|91?|0)?[789][0-9]{9}$/
 const PASSWORD_REGEX = /^(?=.[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
 
+const GOOGLE_SCOPES = [
+  "https://www.googleapis.com/auth/userinfo.profile",
+  "https://www.googleapis.com/auth/userinfo.email",
+]
+
 module.exports = {
   ROLE,
   AVATAR_IMAGE_SIZE,
@@ -23,4 +28,5 @@ module.exports = {
   USERNAME_REGEX,
   PHONE_REGEX,
   PASSWORD_REGEX,
+  GOOGLE_SCOPES
 };
---
+++ src/models/skill.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const skillSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        description: {
+            type: String,
+            trim: true,
+        },
+        basePrice: {
+            type: Number,
+            required: true,
+            trim: true,
+        },
+        image: {
+            type: String,
+            trim: true,
+        },
+        icon: {
+            type: String,
+            trim: true,
+        },
+        popularity: {
+            type: Number,
+            trim: true,
+            default: 0,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Skill = mongoose.model('Skill', skillSchema);
+
+module.exports = Skill;
