706cbb238efd1535a98b0dffbe1768de28145be5
Sankhasubhra Roy <sankha.asansol@gmail.com>
2023-06-23 22:32:58 +0530

    Admin model created

--- src/models/admin.js
+++ src/models/admin.js
@@ -1,4 +1,32 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const adminSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        minlength: 3,
+        required: "Name is required",
+    },
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+        minlength: 3,
+        required: "Username is required",
+    },
+    password: {
+        type: String,
+        trim: true,
+    },
+    avatar: {
+        type: String,
+        trim: true,
+    },
+},
+    {
+        timestamps: true,
+    }
+);
 
+module.exports = mongoose.model("admin", adminSchema);
